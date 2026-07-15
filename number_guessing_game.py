e100aa1338326d2f461b1d80446bcc19b133e952
arcgocode <114783798+ArchitVaksh@users.noreply.github.com>
2026-07-15 17:32:37 +0530

    Add attempt counter to number guessing game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,10 +1,12 @@
 import random
 secret_number = random.randint(1,100)
-
+count = 0
 while True:
+    count += 1
     guess = int(input("Guess a number (1-100): "))
     if guess == secret_number:
         print("Correct!")
+        print("You have guessed the number in", count, "attempts")
         break
     elif guess > secret_number:
         print("Too High!")
