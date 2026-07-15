c6cc61a43788b25f4b21d855e2a39747970167ef
arcgocode <114783798+ArchitVaksh@users.noreply.github.com>
2026-07-15 18:16:45 +0530

    Handle invalid input in number guessing game

--- number_guessing_game.py
+++ number_guessing_game.py
@@ -1,14 +1,17 @@
 import random
 secret_number = random.randint(1,100)
-count = 0
+count = 1
 while True:
-    count += 1
-    guess = int(input("Guess a number (1-100): "))
-    if guess == secret_number:
-        print("Correct!")
-        print("You have guessed the number in", count, "attempts")
-        break
-    elif guess > secret_number:
-        print("Too High!")
-    elif guess < secret_number:
-        print("Too Low!")
+    try:
+        guess = int(input("Guess a number (1-100): "))
+        if guess == secret_number:
+            print("Correct!")
+            print("You have guessed the number in", count, "attempts")
+            break
+        elif guess > secret_number:
+            print("Too High!")
+        elif guess < secret_number:
+            print("Too Low!")
+        count += 1
+    except:
+        print("Invalid input, try again")
